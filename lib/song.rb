5b867c91daadeb4eb8a63df5bb5277d72b358d88
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-17 20:01:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,14 +27,14 @@ class Song
   
   def self.genre_count
     genre_count = {}
-    @@genres.each do |genre|
+    @@genres.each do |genre| 
       if genre_count[genre]
-        genre_conunt[genre] += 1 
-      else 
-        genre_count = 1 
-      end 
-    end 
+      genre_count[genre] += 1 
+      else
+      genre_count[genre] = 1
+      end
+    end
     genre_count
-  end 
+  end
   
 end 
