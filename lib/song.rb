db4f1fcde8de891a5bd8b6f1b045c605b2346eba
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-17 19:55:58 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+class Song 
+  @@count = 0 
+  @@genres = []
+  @@artists = []
+  attr_accessor :name, :artist, :genre 
+  
+  def initialize(name, artist, genre) 
+    @name = name
+    @artist = artist
+    @genre = genre 
+    @@count += 1 
+    @@genres << @genre 
+    @@artists << @artist
+  end 
+  
+  def self.count 
+    @@count 
+  end 
+  
+  def self.genres 
+    @@genres.uniq 
+  end 
+  
+  def self.artists
+    @@artists.uniq 
+  end 
+  
+  def self.genre_count
+  end 
+  
+  
+end 
