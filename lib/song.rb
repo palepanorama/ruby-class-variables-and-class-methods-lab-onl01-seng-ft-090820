85ea88f05795b7248b279cfe424d6a866868257b
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-17 20:03:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,4 +37,14 @@ class Song
     genre_count
   end
   
+  def self.artist_count 
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else 
+        artist_count[artist] =1 
+      end 
+      artist_count
+    end 
 end 
